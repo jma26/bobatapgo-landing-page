c71ca7f9e0cd37b763d367f3b91ee6f76602c7e9
Jesse Ma <j.masonoma17@gmail.com>
2021-09-29 11:57:52 -0700

    Add sliding drawer transition for mobile header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,34 +3,49 @@ import { Link as GatsbyLink } from "gatsby";
 
 import AppBar from "@mui/material/AppBar";
 import Link from '@mui/material/Link';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
 import Typography from "@mui/material/Typography";
 import Toolbar from "@mui/material/Toolbar";
 import Container from "@mui/material/Container";
 import IconButton from "@mui/material/IconButton";
-import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import MenuIcon from "@mui/icons-material/Menu";
 import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
 
 import useMediaQuery from "@mui/material/useMediaQuery";
-import { useTheme } from "@mui/material/styles"
+import { useTheme } from "@mui/material/styles";
+import { makeStyles } from "@mui/styles";
 
-interface HeaderProps {
-  siteTitle: string
-}
+const useStyles = makeStyles({
+  DrawerBackDrop: {
+    '& .MuiBackdrop-root': {
+      backgroundColor: 'transparent'
+    },
+    '& .MuiPaper-root': {
+      backgroundColor: '#37475D',
+      color: '#FFF'
+    }
+  }
+}) 
 
-const Header = ({ siteTitle }: HeaderProps) => {
-  const [anchor, setAnchor] = useState(null);
-  const open = Boolean(anchor);
+const Header = () => {
   const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down('lg'));
+  const classes = useStyles();
+  const [anchor, setAnchorEl] = useState<null | HTMLElement>(null);
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+
   const handleMenu = (event: MouseEvent<HTMLElement>) => {
-    setAnchor(event.currentTarget);
+    setAnchorEl(event.currentTarget);
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null);
   }
 
   return (
     <AppBar position='static'>
-      <Container>
+      <Container disableGutters={true}>
         <Toolbar>
           <Typography
             sx={{ mr: 3, fontSize: '18px' }}
@@ -44,7 +59,7 @@ const Header = ({ siteTitle }: HeaderProps) => {
                   component={GatsbyLink}
                   to='/suport'
                   variant='h6'
-                  sx={{ color: '#FFF', ml: 'auto', mr: '3', fontSize: '18px' }}
+                  sx={{ color: '#FFF', ml: 'auto', mr: 1, fontSize: '18px' }}
                 >
                   Get Started
                 </Link>
@@ -57,40 +72,66 @@ const Header = ({ siteTitle }: HeaderProps) => {
                 >
                   <MenuIcon />
                 </IconButton>
-                <Menu
-                  id='menu-appbar'
-                  anchorEl={anchor}
-                  open={open}
+                <Drawer
+                  anchor='left'
+                  open={Boolean(anchor)}
+                  onClose={handleClose}
+                  className={classes.DrawerBackDrop}
+                  PaperProps={{
+                    style: {
+                      minHeight: '100vh',
+                      minWidth: '100vw',
+                      top: '55px',
+                    }
+                  }}
+                  sx={{ 
+                    height: `calc(100% - 64px)`,
+                    top: 64,
+                    '& .MuiBackdrop-root': {
+                      backgroundColor: 'transparent'
+                    },
+                    '& .MuiPaper-root': {
+                      backgroundColor: theme.palette.primary.main,
+                      color: '#FFF'
+                    }
+                  }}
                 >
                   <MenuItem
-                    onClick={() => setAnchor(null)}
+                    onClick={handleClose}
                     component={GatsbyLink}
                     to='/pricing'
                   >
                     Pricing
                   </MenuItem>
                   <MenuItem
-                    onClick={() => setAnchor(null)}
+                    onClick={handleClose}
                     component={GatsbyLink}
                     to='/support'
                   >
                     Support
                   </MenuItem>
                   <MenuItem
-                    onClick={() => setAnchor(null)}
+                    onClick={handleClose}
                     component={GatsbyLink}
                     to='/contact'
                   >
                     888-888-8888
                   </MenuItem>
                   <MenuItem
-                    onClick={() => setAnchor(null)}
+                    onClick={handleClose}
                     component={GatsbyLink}
                     to='/demo'
                   >
                     Get Started
                   </MenuItem>
-                </Menu>
+                  <MenuItem
+                    onClick={handleClose}
+                    component={GatsbyLink}
+                    to='/login'
+                  >
+                    Login
+                  </MenuItem>
+                </Drawer>
               </>
             ) : (
               <>
@@ -98,7 +139,7 @@ const Header = ({ siteTitle }: HeaderProps) => {
                   component={GatsbyLink}
                   to='/pricing'
                   variant='h6'
-                  sx={{ color: '#FFF', mx: 3, fontSize: '18px' }}
+                  sx={{ color: '#FFF', mx: 4, fontSize: '18px' }}
                 >
                   Pricing
                 </Link>
@@ -106,7 +147,7 @@ const Header = ({ siteTitle }: HeaderProps) => {
                   component={GatsbyLink}
                   to='/suport'
                   variant='h6'
-                  sx={{ color: '#FFF', mx: 3, fontSize: '18px' }}
+                  sx={{ color: '#FFF', mx: 4, fontSize: '18px' }}
                 >
                   Support
                 </Link>
@@ -114,22 +155,24 @@ const Header = ({ siteTitle }: HeaderProps) => {
                   component={GatsbyLink}
                   to='/suport'
                   variant='h6'
-                  sx={{ color: '#FFF', mx: 3, fontSize: '18px' }}
+                  sx={{ color: '#FFF', mx: 4, fontSize: '18px' }}
                 >
                   888-888-8888
                 </Link>
-                <Link
+                <Button
                   component={GatsbyLink}
                   to='/demo'
-                  variant='h6'
-                  sx={{ color: '#FFF', ml: 'auto', fontSize: '18px' }}
+                  color='secondary'
+                  variant='contained'
+                  disableElevation
+                  sx={{ ml: 'auto', borderRadius: '2em' }}
                 >
                   Get Started
-                </Link>
+                </Button>
                 <Link
                   component={GatsbyLink}
                   to='/login'
-                  sx={{ color: '#FFF', ml: 3 }}
+                  sx={{ color: '#FFF', ml: 4, borderLeft: '2px solid #FFF', paddingLeft: 4 }}
                 >
                   <PersonOutlineIcon />
                 </Link>
